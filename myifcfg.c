a4733f88f0ceb422777d387535c78cf9fd8a859f
Paul DeLong <Paul.DeLong@gmail.com>
2017-06-22 18:11:24 -0400

    moved loop body into function definition
    The body of the loop iterating over the devices has been moved into its
    own function, for better structuring.  It also had a side-effect of
    alerting me to a bug in the way I was processing the flags (which I
    fixed).

--- myifcfg.c
+++ myifcfg.c
@@ -7,6 +7,49 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+void process_device( pcap_if_t *dev_p ) {
+
+   printf( "name: %s\n", dev_p->name );
+
+   if( dev_p->description != NULL ) {
+      printf( "description: %s\n", dev_p->description );
+   }
+
+   pcap_addr_t *address = dev_p->addresses;
+
+   while( address != NULL ) {
+
+      struct sockaddr_in *addr = (struct sockaddr_in *)address->addr;
+
+      if( addr->sin_family != AF_INET ) {
+         pcap_addr_t *temp_addr_p = address->next;
+         address = temp_addr_p;
+         continue;
+      }
+
+      char *ip = inet_ntoa( addr->sin_addr );
+
+      if( ip == NULL ) {
+         perror( "inet_ntoa" );
+         exit( EXIT_FAILURE );
+      }
+
+      printf( "\tIP: %s\n", ip );
+
+      pcap_addr_t *temp_addr_p = address->next;
+      address = temp_addr_p;
+   }
+
+   int flags = dev_p->flags;
+
+   if( flags )                    printf( "\tflags:" );
+   if( flags & PCAP_IF_LOOPBACK ) printf( " LOOPBACK" );
+   //if( flags & PCAP_IF_UP )       printf( " UP" );
+   //if( flags & PCAP_IF_RUNNING )  printf( " RUNNING" );
+   // the manpage lies, on MacOS anyhow
+   printf( "\n" );
+}
+
 int main( int argc, char **argv ) {
 
    char errbuf[PCAP_ERRBUF_SIZE];
@@ -24,48 +67,10 @@ int main( int argc, char **argv ) {
 
    while( current_dev_p != NULL ) {
 
-      printf( "name: %s\n", current_dev_p->name );
-
-      if( current_dev_p->description != NULL ) {
-         printf( "description: %s\n", current_dev_p->description );
-      }
-
-      pcap_addr_t *address = current_dev_p->addresses;
-
-      while( address != NULL ) {
-
-         struct sockaddr_in *addr = (struct sockaddr_in *)address->addr;
-
-         if( addr->sin_family != AF_INET ) {
-            pcap_addr_t *temp_addr_p = address->next;
-            address = temp_addr_p;
-            continue;
-         }
-
-         char *ip = inet_ntoa( addr->sin_addr );
-
-         if( ip == NULL ) {
-            perror( "inet_ntoa" );
-            exit( EXIT_FAILURE );
-         }
-
-         printf( "\tIP: %s", ip );
-
-         pcap_addr_t *temp_addr_p = address->next;
-         address = temp_addr_p;
-      }
-
+      process_device( current_dev_p );
+   
       pcap_if_t *temp_dev_p = current_dev_p->next;
       current_dev_p = temp_dev_p;
-
-      int flags = current_dev_p->flags;
-
-      if( flags )                    printf( "\tflags:" );
-      if( flags & PCAP_IF_LOOPBACK ) printf( " LOOPBACK" );
-      //if( flags & PCAP_IF_UP )       printf( " UP" );
-      //if( flags & PCAP_IF_RUNNING )  printf( " RUNNING" );
-      // the manpage lies, on MacOS anyhow
-      printf( "\n" );
    }
 
    pcap_freealldevs( first_dev_p );
