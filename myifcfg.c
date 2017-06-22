41b673a8176298c30a7f2373971db6b5b83ea628
Paul DeLong <Paul.DeLong@gmail.com>
2017-06-22 17:59:09 -0400

    making my own ifconfig, as an exercise;

--- myifcfg.c
+++ myifcfg.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pcap.h>
+#include <pcap/pcap.h>
 #include <errno.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -9,46 +10,65 @@
 int main( int argc, char **argv ) {
 
    char errbuf[PCAP_ERRBUF_SIZE];
-   bpf_u_int32 netp, maskp;
 
-   char *dev = pcap_lookupdev( errbuf );
+   pcap_if_t *current_dev_p;
 
-   if( dev == NULL ) {
+   int ret = pcap_findalldevs( &current_dev_p, errbuf );
+
+   pcap_if_t *first_dev_p = current_dev_p;
+
+   if( ret == -1 || ret != 0 ) {
       printf( "ERROR: %s - aborting\n", errbuf );
       exit( EXIT_FAILURE );
    }
 
-   printf( "device = %s\n", dev );
+   while( current_dev_p != NULL ) {
 
-   int ret = pcap_lookupnet( dev, &netp, &maskp, errbuf );
+      printf( "name: %s\n", current_dev_p->name );
 
-   if( ret == -1 ) {
-      printf( "ERROR: %s - aborting\n", errbuf );
-      exit( EXIT_FAILURE );
+      if( current_dev_p->description != NULL ) {
+         printf( "description: %s\n", current_dev_p->description );
+      }
+
+      pcap_addr_t *address = current_dev_p->addresses;
+
+      while( address != NULL ) {
+
+         struct sockaddr_in *addr = (struct sockaddr_in *)address->addr;
+
+         if( addr->sin_family != AF_INET ) {
+            pcap_addr_t *temp_addr_p = address->next;
+            address = temp_addr_p;
+            continue;
+         }
+
+         char *ip = inet_ntoa( addr->sin_addr );
+
+         if( ip == NULL ) {
+            perror( "inet_ntoa" );
+            exit( EXIT_FAILURE );
+         }
+
+         printf( "\tIP: %s", ip );
+
+         pcap_addr_t *temp_addr_p = address->next;
+         address = temp_addr_p;
+      }
+
+      pcap_if_t *temp_dev_p = current_dev_p->next;
+      current_dev_p = temp_dev_p;
+
+      int flags = current_dev_p->flags;
+
+      if( flags )                    printf( "\tflags:" );
+      if( flags & PCAP_IF_LOOPBACK ) printf( " LOOPBACK" );
+      //if( flags & PCAP_IF_UP )       printf( " UP" );
+      //if( flags & PCAP_IF_RUNNING )  printf( " RUNNING" );
+      // the manpage lies, on MacOS anyhow
+      printf( "\n" );
    }
 
-   struct in_addr addr;
-
-   addr.s_addr = netp;
-
-   char *net = inet_ntoa( addr );
-
-   if( net == NULL ) {
-      perror( "inet_ntoa" );
-      exit( EXIT_FAILURE );
-   }
-
-   printf( "network = %s\n", net );
-
-   addr.s_addr = maskp;
-   char *mask = inet_ntoa( addr );
-
-   if( mask == NULL ) {
-      perror( "inet_ntoa" );
-      exit( EXIT_FAILURE );
-   }
-  
-   printf( "netmask = %s\n", mask );
+   pcap_freealldevs( first_dev_p );
 
    exit( EXIT_SUCCESS );
 }
