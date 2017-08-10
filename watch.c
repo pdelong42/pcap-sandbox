c8f4ebb577f4c7aed326ebe6dc6ce96009e5d262
Paul DeLong <Paul.DeLong@gmail.com>
2017-08-10 15:36:02 -0400

    [feature/live-loop] printing the MAC addresses (part 2)
    The last commit covered MacOS - this one covers Linux.

--- watch.c
+++ watch.c
@@ -5,6 +5,20 @@
 #include <pcap/pcap.h>
 #include <net/ethernet.h>
 
+#ifdef __APPLE__
+#   include <net/if_dl.h>
+#   define AF_CUSTOM1 AF_LINK
+#   define MAC_NTOA link_ntoa
+#   define MAC_NTOA_STR "link_ntoa"
+#   define MAC_SOCKADDR sockaddr_dl
+#else
+#   include <netinet/ether.h>
+#   define AF_CUSTOM1 AF_PACKET
+#   define MAC_NTOA ether_ntoa
+#   define MAC_NTOA_STR "ether_ntoa"
+#   define MAC_SOCKADDR ether_addr
+#endif
+
 static int count = 0;
 
 void callback( u_char *useless,
